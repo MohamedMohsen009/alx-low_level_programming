2a0162ee1255f46ce90ed32bf9a3c7709f3d3686
Mohamed Mohsen <mohamedmoihsen@gmail.com>
2024-04-14 01:47:21 +0000

    Added 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	char c;
+	long int l;
+	long long int o;
+	float f;
+
+	printf("Size of a char:%d  byte(s) \n",sizeof(c));
+}
