f6859ecaeef6d18ead72fbef7ee553cb647a76d7
Mohamed Mohsen <mohamedmoihsen@gmail.com>
2024-04-15 04:54:48 +0000

    Thats A NEW FILE

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry Point
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	int i = 90;
+
+	while (i <= 122)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
