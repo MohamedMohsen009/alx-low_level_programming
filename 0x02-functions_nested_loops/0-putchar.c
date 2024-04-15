0168d2c1370582e70d628f65deb2fd14338f7f24
Mohamed Mohsen <mohamedmoihsen@gmail.com>
2024-04-15 04:41:49 +0000

    added a new file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * main - Entry Point
@@ -8,12 +7,13 @@
 int main(void)
 {
 	_putchar('_');
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+	return (0);
 }
