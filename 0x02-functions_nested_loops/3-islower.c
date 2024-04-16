a5176e742615f8ef5231a788215b0a82a1bd235e
Mohamed Mohsen <mohamedmoihsen@gmail.com>
2024-04-16 00:37:51 +0000

    added a new file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - checks if a char is lowercase.
+ *
+ * Return: 1 if lower and 0 if not.
+ */
+int _islower(int c)
+{
+	if (c <= 122 && c >= 97)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
