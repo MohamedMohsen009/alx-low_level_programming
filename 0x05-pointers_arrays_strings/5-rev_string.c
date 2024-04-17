16a370b402238b8f9655edcb004cc4decfe4fa3e
Mohamed Mohsen <mohamedmoihsen@gmail.com>
2024-04-17 00:24:28 +0000

    added a new file

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * rev_string - reverses a string.
+ * @*s: the string to be reversed.
+ * Return : void
+ */
+void rev_string(char *s)
+{
+	int length = 0;
+	int index;
+	char temp;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	for (index = 0; index < length / 2; index++)
+	{
+		temp = s[index];
+		s[index] = s[length - index - 1];
+		s[length - index - 1] = temp;
+	}
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
+
+void rev_string(char *s);
+
+#endif
