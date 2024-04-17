783ff083d29b4d58c685bce234e9c37d0d374093
Mohamed Mohsen <mohamedmoihsen@gmail.com>
2024-04-17 03:33:58 +0000

    added a new file

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints n elements of array.
+ * @a: the array.
+ * @n: the number of elements.
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int length = 0;
+	int index;
+
+	while (a[length] != '\0')
+	{
+		length++;
+	}
+	if (n <= length)
+	{
+		for (index = 0; index <= n; index++)
+		{
+			printf("%d, ",a[index]);
+		}
+	}
+}	
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,5 +4,6 @@
 void puts_half(char *str);
 void puts2(char *str);
 void rev_string(char *s);
+void print_array(int *a, int n);
 
 #endif
