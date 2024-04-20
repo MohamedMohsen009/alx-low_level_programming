38f16ae558a3033e76c6e83072d876442b7b6103
Mohamed Mohsen <mohamedmoihsen@gmail.com>
2024-04-20 09:22:23 +0000

    added a new file

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,9 +10,14 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int l;
 
-	for (l = 0; l < n; l++)
+	for (l = 0; l < n && src[l] != '\0'; l++)
 	{
 		dest[l] = src[l];
+	}
+	while (l < n)
+	{
+		dest[l] = '\0';
+		l++;
 	}
 	return (dest);
 }
