16ea96a1e17788563922630e77b6cadf7f74bf58
Mohamed Mohsen <mohamedmoihsen@gmail.com>
2024-04-20 07:51:07 +0000

    added a new file

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings.
+ * @dest: string 1.
+ * @src: string 2.
+ * @n: number of bytes.
+ * Return: a pointer to dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0'; i++);
+
+	for (j = 0; src[j] != '\0' && j < n; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	if (j == n - 1)
+	{
+		dest[i + j] = '\0';
+	}
+	return (dest);
+}
