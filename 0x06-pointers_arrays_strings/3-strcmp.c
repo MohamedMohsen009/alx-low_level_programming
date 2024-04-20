5f315f68c7f964b26660a49ab9a0629769628599
Mohamed Mohsen <mohamedmoihsen@gmail.com>
2024-04-20 09:42:34 +0000

    added a file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,21 +7,18 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int result = 0;
 	int l;
 
 	for (l = 0; s1[l] != '\0' && s2[l] != '\0'; l++)
 	{
 		if (s1[l] > s2[l])
 		{
-			result = 15;
 			break;
 		}
 		else if (s1[l] < s2[l])
 		{
-			result = -15;
 			break;
 		}
 	}
-	return (result);
+	return (s1[l] - s2[l]);
 }
