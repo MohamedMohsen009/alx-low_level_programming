c80952cb82f7eea07a3e3612d7396219627c845a
Mohamed Mohsen <mohamedmoihsen@gmail.com>
2024-04-21 05:53:36 +0000

    added a new file

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string.
+ * @str: a string.
+ * Return: a pointer to a string.
+ */
+char *cap_string(char *str)
+{
+	int i;
+	int s;
+
+	i = 0;
+	s = 0;
+	while (str[i] != '\0')
+	{
+		if (s == 1 && str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+			s = 0;
+		}
+		else if (str[i] == ',' || str[i] == '\t' || str[i] == ';' || str[i] == '.' || str[i] == 32 || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+		{
+			s = 1;
+		}
+		else
+		{
+			s = 0;
+		}
+		i++;
+	}
+	return (str);
+}
