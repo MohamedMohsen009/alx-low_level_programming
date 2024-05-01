af00bbbaa7dd5f5b2b74e9eb8ac1a2047509d463
Mohamed Mohsen <mohamedmoihsen@gmail.com>
2024-05-01 21:21:43 +0000

    added a new file

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stddef.h>
+
 /**
  * strchr - locates a character in a string.
  * @s: a string.
@@ -16,5 +16,5 @@ char *_strchr(char *s, char c)
 		}
 		s++;
 	}
-	return NULL;
+	return ('\0');
 }
