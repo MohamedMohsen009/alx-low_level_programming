2a234df3a0c4862b402467c991548f740ad4ec6a
Mohamed Mohsen <mohamedmoihsen@gmail.com>
2024-06-06 11:56:38 +0000

    added a new file

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * int _pow_recursion - a function that returns the value of x raised to y.
+ * _pow_recursion - a function that returns the value of x raised to y.
  * @x: an int.
  * @y: an int.
  * Return: an int.
@@ -17,4 +17,4 @@ int _pow_recursion(int x, int y)
 		return (1);
 	}
 	return (x * _pow_recursion(x, y - 1));
-}	
+}
