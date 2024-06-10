9aa14406e9c3b2e6bab1997cc42e3804a04e965c
Mohamed Mohsen <mohamedmoihsen@gmail.com>
2024-06-10 03:37:08 +0000

    added a new file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ ** main - prints the program name, followed by a new line
+ ** @argc: the number of command line arguments
+ ** @argv: an array of strings containing the command line arguments
+ **
+ ** Return: 0 on success
+ **/
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s", argv[0]);
+	return (0);
+}
