33a5cd949df222a1b30c7be4ce845b0c4df2bf41
Mohamed Mohsen <mohamedmoihsen@gmail.com>
2024-06-10 18:59:18 +0000

    added a new file

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * main - the start of the promgram.
+ * @argc: the count of the arguments.
+ * @argv: an array holding the arguments.
+ *
+ * Return: always (0) SUCCESS.
+ */
 int main(int argc, char *argv[])
 {
 	(void)argv;
