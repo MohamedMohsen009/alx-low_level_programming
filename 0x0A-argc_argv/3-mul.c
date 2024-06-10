2b6047353a072841519e17d32dc0959746e740a2
Mohamed Mohsen <mohamedmoihsen@gmail.com>
2024-06-10 19:17:17 +0000

    added a new file

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point of the program.
+ * @argc: count of the arguments.
+ * @argv: an array holding all the arguments.
+ *
+ * Return: always (0) SUCCESS.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc < 3 || argc > 3)
+	{
+		printf("%s\n", "ERROR");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
