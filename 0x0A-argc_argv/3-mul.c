b2f52d162f2957eefa20441943c9edfd5f9a291e
Mohamed Mohsen <mohamedmoihsen@gmail.com>
2024-06-10 19:17:59 +0000

    added a new file

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 {
 	if (argc < 3 || argc > 3)
 	{
-		printf("%s\n", "ERROR");
+		printf("%s\n", "Error");
 		return (1);
 	}
 	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
