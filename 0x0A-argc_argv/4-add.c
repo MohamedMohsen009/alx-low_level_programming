7389db9b8b6724e08892f1985f4dacebe2ce0d5c
Mohamed Mohsen <mohamedmoihsen@gmail.com>
2024-06-12 01:14:41 +0000

    added a new file

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point of the program.
+ * @argc: count of the arguments.
+ * @argv: an array holding the arguments.
+ *
+ * Return: always 0 (SUCCEESS).
+ */
+int main(int argc, char *argv[])
+{
+	int result;
+
+	result = 0;
+	if (argc <= 1)
+	{
+		printf ("%d\n", 0);
+		return (0);
+	}
+	while (argc--)
+	{
+		if (argc > 0)
+		{
+			if (atoi(argv[argc]) != 0)
+			{
+				result += atoi(argv[argc]);
+			}
+			else
+			{
+				printf("%s\n", "Error");
+				return (0);
+			}
+		}
+	}
+	printf("%d\n", result);
+	return (0);
+}
