dc43e6cc3b5bef121e90a7181baa7d0846274284
Mohamed Mohsen <mohamedmoihsen@gmail.com>
2024-06-16 21:32:44 +0000

    added a file

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -29,6 +29,11 @@ int **alloc_grid(int width, int height)
 		arr[i] = malloc(width * sizeof(int));
 		if (arr[i] == NULL)
 		{
+			for (j = 0; j < i; j++)
+			{
+				free(arr[i]);
+			}
+			free(arr);
 			return (NULL);
 		}
 		for (j = 0; j < width; j++)
