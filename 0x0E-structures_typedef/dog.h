829827cab8a63219f4247ebd713332b37ac3e943
Mohamed Mohsen <mohamedmoihsen@gmail.com>
2024-07-05 23:03:03 +0000

    added a new file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
+/**
+ * struct dog - a structure that holds data about the dog owner.
+ * @name: the name of the dog.
+ * @age: the age of the owner.
+ * @owner: the name of the owner.
+ *
+ * @Description: a struct that holds the data of the dog and its owner.
+ */
+struct dog
+{
+	char* name;
+	float age;
+	char* owner;
+}
+#endif
