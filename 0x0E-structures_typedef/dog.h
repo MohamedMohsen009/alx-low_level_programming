0f82d821bfc1103bfa86c965f63e14719b581361
Mohamed Mohsen <mohamedmoihsen@gmail.com>
2024-07-05 23:11:27 +0000

    added a new file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,7 +3,7 @@
 /**
  * struct dog - a structure that holds data about the dog owner.
  * @name: the name of the dog.
- * @age: the age of the owner.
+ * @age: the age of the dog.
  * @owner: the name of the owner.
  *
  * @Description: a struct that holds the data of the dog and its owner.
