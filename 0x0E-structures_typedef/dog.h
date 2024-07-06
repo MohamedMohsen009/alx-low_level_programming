353eae8c7c78572ca2b2ad1d80d75f56858ca9fb
Mohamed Mohsen <mohamedmoihsen@gmail.com>
2024-07-06 18:18:23 +0000

    added a new file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,12 @@
  *
  * Description: a struct that holds the data of the dog and its owner.
  */
-struct dog
+typedef struct
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
